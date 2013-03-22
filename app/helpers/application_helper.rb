53637b3e4b060fc52f94a870557a3409cba50ff5
Dmitry Malakhov <dmitrii.malakhov@gmail.com>
2013-03-22 10:22:49 +0400

    logo png

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
 	def logo
-		image_tag("logo.jpg", :alt => "QuickTouch")
+		image_tag("logo.png", :alt => "QuickTouch")
 	end
 	def partner_banner_path
 		return String.new("partner_promo_#{I18n.locale}.png")
