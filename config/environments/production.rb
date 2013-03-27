f75ad7beb1c0f1b8cea93535634fd471cff690f2
Dmitry Malakhov <dmitrii.malakhov@gmail.com>
2013-03-27 15:32:02 +0400

    google-analytic _2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,8 +61,6 @@ Quicktouch::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  GA.tracker = "UA-36932968-1"
-
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
