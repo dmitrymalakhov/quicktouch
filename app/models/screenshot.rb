b83dcdc6c89f38e5bc8a73c2b8157f27d62d32e6
Dmitry M <dmitrii.malakhov@gmail.com>
2013-10-23 22:15:38 +0400

    fix attr_accessible :image to screenshot.rb

--- app/models/screenshot.rb
+++ app/models/screenshot.rb
@@ -1,10 +1,11 @@
 class Screenshot < ActiveRecord::Base
-  belongs_to :application
-  has_attached_file :image,
-    :styles => {
-      :thumb=> "100x100#",
-      :small  => "300x300>",
-      :large => "600x600>"
-    }
-  # attr_accessible :title, :body
+	attr_accessible :image
+	belongs_to :application
+	has_attached_file :image,
+	:styles => {
+	  :thumb=> "100x100#",
+	  :small  => "300x300>",
+	  :large => "600x600>"
+	}
+# attr_accessible :title, :body
 end
