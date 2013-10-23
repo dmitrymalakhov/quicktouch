483c6bb60d50e60bddc681f248090a79c3576c47
Dmitry M <dmitrii.malakhov@gmail.com>
2013-10-23 22:10:37 +0400

    fix attach

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,8 +1,8 @@
 class Application < ActiveRecord::Base
-  	attr_accessible :android_link, :description, :ios_link, :locale, :title, :photo, :web_link
+  	attr_accessible :android_link, :description, :ios_link, :locale, :title, :photo, :web_link, :screenshots_attributes
 
   	has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 
   	has_many :screenshots, :dependent => :destroy
-	accepts_nested_attributes_for :screenshots, :allow_destroy => true
+	accepts_nested_attributes_for :screenshots, :allow_destroy => true, :reject_if => :all_blank
 end
